79717eda76221ef1e8c059cfea1809706a326d0f
reguiegyounes@gmail.com <reguiegyounes@gmail.com>
2021-01-05 17:50:04 +0100

    commit create routes(users,exercises) Part02

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -25,4 +25,32 @@ router.post('/add',(req, res) => {
   .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.get('/:id',(req, res) => {
+    Exercise.findById(req.params.id)
+      .then(exercise => res.json(exercise))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+  
+  router.delete('/:id',(req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+      .then(() => res.json('Exercise deleted.'))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+  
+  router.post('/update/:id',(req, res) => {
+    Exercise.findById(req.params.id)
+      .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
+  
+        exercise.save()
+          .then(() => res.json('Exercise updated!'))
+          .catch(err => res.status(400).json('Error: ' + err));
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
+
 module.exports=router;
